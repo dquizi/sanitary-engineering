5df7440f323a72168c7041b705806e1c03c9e067
dquizi <quizi@bk.ru>
2019-06-17 02:12:06 +0300

    Правки в 1 и 2 секцию, сверстал секцию service

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+/* Настройки бургер-меню */
+$('.menu-btn').on('click', function(e) {
+	e.preventDefault;
+	$(this).toggleClass('menu-btn_active');
+});
+
+/* Инициализируем Owl */
+// var owl = $('.owl-carousel');
+$(document).ready(function () {
+	$('.owl-carousel').owlCarousel({
+		items: 1,
+		loop: true,
+		dots: true,
+		dotsEach: true,
+		dotsData: true,
+		// dotsContainer: true,
+		autoplay: false,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: false,
+	})
+});
