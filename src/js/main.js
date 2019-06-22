ebefab547dc858d8a125ff5c89555ef0c81e0b11
dquizi <quizi@bk.ru>
2019-06-23 01:34:49 +0300

    Правки, правки, правки + БУРГЕР-МЕНЮ

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 /* Настройки бургер-меню */
+
 $('.menu-btn').on('click', function(e) {
 	e.preventDefault;
-	$(this).toggleClass('menu-btn_active');
+	// $(this).toggleClass('menu-btn_active');
+	$('.menu-btn_active').fadeToggle('menu-btn_active');
+	// $('.header-block__menu').toggleClass('header-block__menu_active');
 });
 
 /* Инициализируем Owl */
@@ -13,9 +16,38 @@ $(document).ready(function () {
 		dots: true,
 		dotsEach: true,
 		dotsData: true,
-		dotsContainer: true,
 		autoplay: false,
+		nav: true,
 		autoplayTimeout: 5000,
 		autoplayHoverPause: false,
 	})
 });
+
+// $('.service-wrap__title').on('hover', function() {
+// 	$('.price')
+// })
+
+$('#geo-form').validate({
+	rules: {
+		username: {
+			required: true,
+			minlength: 3,
+			maxlength: 15
+		},
+		phone: {
+			required: true,
+		},
+	},
+	errorClass: "invalid",
+	errorElement: "div",
+	messages: {
+		username: {
+			minlength: jQuery.validator.format("Не менее {0} символов"),
+			maxlength: jQuery.validator.format("Введите не более 15 символов")
+		},
+	}
+})
+
+/* Маски телефона */
+$('#phone').mask('8 (999) 999-99-99')
+
--- src/js/map.js
+++ src/js/map.js
@@ -1,12 +1,12 @@
-ymaps.ready(function () {
-	var map = new ymaps.Map("map", {
-		center: [55.76, 37.64],
-		zoom: 10
-	});
-
-	if (map) {
-		ymaps.modules.require(['Placemark', 'Circle'], function (Placemark, Circle) {
-			var placemark = new Placemark([55.37, 35.45]);
-		});
-	}
+$(document).ready(function() {
+  $(window).scroll(function () {
+    if($(window).scrollTop() > 500) {
+      if($(window).width() > 567) {
+        $('.geo-map > script').attr('src', 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A2478c4c01712ab54115e04bd4d7b4c54442ad4086e1b56603cf6551bab884934&amp;width=100%25&amp;height=666&amp;lang=ru_RU&amp;scroll=false');
+      }
+      else {
+        $('.geo-map > script').attr('src', 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A2478c4c01712ab54115e04bd4d7b4c54442ad4086e1b56603cf6551bab884934&amp;width=100%25&amp;height=666&amp;lang=ru_RU&amp;scroll=false');
+      }
+    }
+  });
 });
