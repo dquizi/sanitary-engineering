7285e07524bb46a361dcd69dd6f75f14fe073877
dquizi <quizi@bk.ru>
2019-06-25 00:58:37 +0300

    ВТОРАЯ ПАЧКА ПРАВОК куратора

--- src/js/main.js
+++ src/js/main.js
@@ -2,30 +2,52 @@
 
 $('.menu-btn').on('click', function(e) {
 	e.preventDefault;
-	// $(this).toggleClass('menu-btn_active');
 	$('.menu-btn_active').fadeToggle('menu-btn_active');
-	// $('.header-block__menu').toggleClass('header-block__menu_active');
 });
 
-/* Инициализируем Owl */
-// var owl = $('.owl-carousel');
+/* Настройки Owl для 1 слайдера*/
+
 $(document).ready(function () {
-	$('.owl-carousel').owlCarousel({
+	$('.slider-block').owlCarousel({
 		items: 1,
 		loop: true,
 		dots: true,
 		dotsEach: true,
-		dotsData: true,
-		autoplay: false,
-		nav: true,
+		autoplay: true,
 		autoplayTimeout: 5000,
 		autoplayHoverPause: false,
 	})
 });
 
-// $('.service-wrap__title').on('hover', function() {
-// 	$('.price')
-// })
+/* Настройки Owl для 2 слайдера*/
+
+$(document).ready(function () {
+	$('.feedback-sliders').owlCarousel({
+		items: 1,
+		loop: true,
+		dots: true,
+		dotsEach: true,
+		autoplay: false
+	})
+});
+
+var success = document.querySelector('#success');
+var autoclose = function () {
+	$(success).removeClass('success_active');
+};
+var close = document.querySelector('#close');
+var modal = document.querySelector('#modal');
+var modal_close = document.querySelector('#modal_close');
+var modal_username = document.querySelector('#modal_username');
+var modal_phone = document.querySelector('#modal_phone');
+
+$('.header-block__button').on('click', function () {
+	$('#modal').addClass('modal_active');
+})
+
+modal_close.addEventListener('click', function () {
+	modal.classList.remove('modal_active')
+})
 
 $('#geo-form').validate({
 	rules: {
@@ -40,6 +62,10 @@ $('#geo-form').validate({
 	},
 	errorClass: "invalid",
 	errorElement: "div",
+	submitHandler: function (form) {
+			$('#success').addClass('success_active');
+			setTimeout(autoclose, 4000)
+	},
 	messages: {
 		username: {
 			minlength: jQuery.validator.format("Не менее {0} символов"),
@@ -48,6 +74,46 @@ $('#geo-form').validate({
 	}
 })
 
-/* Маски телефона */
-$('#phone').mask('8 (999) 999-99-99')
+$('#modal-form').validate({
+	rules: {
+		username: {
+			required: true,
+			minlength: 3,
+			maxlength: 15
+		},
+		phone: {
+			required: true,
+		},
+	},
+	errorClass: "invalid",
+	errorElement: "div",
+	submitHandler: function (form) {
+		$('#success').addClass('success_active');
+		setTimeout(autoclose, 4000)
+	},
+	messages: {
+		username: {
+			minlength: jQuery.validator.format("Не менее {0} символов"),
+			maxlength: jQuery.validator.format("Введите не более 15 символов")
+		},
+	}
+})
 
+ $(document).ready(function () {
+ 	$(modal_username).click(function () {
+ 		if ($(this).hasClass("invalid"))
+ 			$("modal").submit();
+ 	})
+ })
+
+close.addEventListener('click', function() {
+	success.classList.remove('success_active')
+})
+
+jQuery.extend(jQuery.validator.messages, {
+	required: "Заполните поле",
+});
+
+/* Маски телефона */
+$('#geo_phone').mask('8 (999) 999-99-99')
+$('#modal_phone').mask('8 (999) 999-99-99')
